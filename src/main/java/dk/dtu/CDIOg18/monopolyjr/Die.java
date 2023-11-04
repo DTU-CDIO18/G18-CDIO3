da6b3270b41a3846cc818d211e538e01b48ea799
Daniel Lerche <daniel.lerche10@gmail.com>
2023-11-04 16:07:49 +0100

    Added Die class and constructor

---
+++ src/main/java/dk/dtu/CDIOg18/monopolyjr/Die.java
@@ -0,0 +1,9 @@
+package dk.dtu.CDIOg18.monopolyjr;
+
+public class Die {
+    private int numberOfSides;
+
+    public Die(int numberOfSides) {
+        this.numberOfSides = numberOfSides;
+    }
+}
