c11228ec0cd7107ecd7e376175e8611f9577db5d
Daniel Lerche <daniel.lerche10@gmail.com>
2023-11-04 16:01:15 +0100

    Added account class with constructor and fields

---
+++ src/main/java/dk/dtu/CDIOg18/monopolyjr/Account.java
@@ -0,0 +1,10 @@
+package dk.dtu.CDIOg18.monopolyjr;
+
+public class Account {
+    
+    private double balance = 0;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+}
