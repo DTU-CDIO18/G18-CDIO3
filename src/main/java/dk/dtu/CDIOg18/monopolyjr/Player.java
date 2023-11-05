9b85e46a3e701596d1d937c40d5f81dbb750ce5a
Daniel Lerche <daniel.lerche10@gmail.com>
2023-11-05 12:43:17 +0100

    Added Player class with constructor and field variables

---
+++ src/main/java/dk/dtu/CDIOg18/monopolyjr/Player.java
@@ -0,0 +1,14 @@
+package dk.dtu.CDIOg18.monopolyjr;
+
+public class Player {
+    
+    private String name;
+    private int age;
+    private Account account;
+
+    public Player(String name, int age, Account account) {
+        this.name = name;
+        this.age = age;
+        this.account = account;
+    }
+}
