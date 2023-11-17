6500027836d097e9bca1d1ae70082f02ceeb9b51
rasmus <rasmus.b.bodker@gmail.com>
2023-11-17 11:40:37 +0100

    Revert "Merge pull request #3 from DTU-CDIO18/bank"
    This reverts commit f487de81c8d16fdb86b97914ceb541e489b327f6, reversing
    changes made to c64df513ca9f3d783da56f8b2c45170c44f228d0.

--- src/main/java/dk/dtu/CDIOg18/monopolyjr/Die.java
+++
@@ -1,9 +0,0 @@
-package dk.dtu.CDIOg18.monopolyjr;
-
-public class Die {
-    private int faceCount = 6;
-
-    public int getFaceValue() {
-        return (int) (Math.random() * this.faceCount) + 1;
-    }
-}
--- src/test/java/dk/dtu/CDIOg18/monopolyjr/FieldTest.java
+++
@@ -1,7 +0,0 @@
-package dk.dtu.CDIOg18.monopolyjr;
-
-public class FieldTest {
-    
-    
-
-}
