069aeffee210dc6d02a9a9d1e481195c36707f50
Daniel Lerche <daniel.lerche10@gmail.com>
2023-11-24 15:55:38 +0100

    Added candy

--- src/test/java/dk/dtu/CDIOg18/monopolyjr/AccountTest.java
+++ src/test/java/dk/dtu/CDIOg18/monopolyjr/AccountTest.java
@@ -2,12 +2,15 @@ package dk.dtu.CDIOg18.monopolyjr;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 public class AccountTest {
-    
+
     private final String playerName = "Johnny Test";
     private final int playerAge = 10;
     private final Token token = Token.BOAT;
@@ -15,27 +18,31 @@ public class AccountTest {
 
     private Player player;
 
-    @BeforeEach
     public void setup() {
         Account account = new Account(startingBalance);
         player = new Player(playerName, playerAge, token, account);
     }
 
+    @Test
     public void shouldNotGoInMinusWhenWithdrawAndShouldReturnFalse() {
+        setup();
         // Test for "off by one error"
-        assertFalse(player.getAccount().withdraw(startingBalance - 1));
-        assertEquals(0, player.getAccount().getBalance());
+        assertFalse(player.getAccount().withdraw(startingBalance + 1));
+        assertEquals(startingBalance, player.getAccount().getBalance());
     }
 
+    @Test
     public void shouldNotReturnFalseWhenAccountIsZero() {
+        setup();
         assertTrue(player.getAccount().withdraw(startingBalance));
     }
 
+    @Test
     public void shouldNotAllowAccountDepositAndWithdrawWithNegativeInput() {
-        double negativeAmount = -10;
-        player.getAccount().deposit(negativeAmount);
-        assertEquals(player.getAccount(), startingBalance);
-        player.getAccount().withdraw(negativeAmount);
-        assertEquals(player.getAccount(), startingBalance);
+        setup();
+        double negativeAmount = -1;
+        assertThrows(IllegalArgumentException.class, () -> player.getAccount().withdraw(negativeAmount));
+        assertThrows(IllegalArgumentException.class, () -> player.getAccount().deposit(negativeAmount));
+        assertEquals(startingBalance, player.getAccount().getBalance());
     }
 }
